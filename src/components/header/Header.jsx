8ff0ad592ce85ea9605eb73a43a04e5acb77dcb9
salihtzc <salihtzc.1997@gmail.com>
2022-07-07 12:36:20 +0200

    r

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import "./header.scss"
+
+const Header =()=> {
+return(
+    <header>
+      <div className="header_1"></div>
+        <a>ABOUT US</a>
+        <a>FOR YOU</a>
+        <a>SERVICES</a>
+        <a>BLOG</a>
+        <a>VLOG</a>
+        <a>CONTACT</a>
+
+    </header>
+);
+
+};
+
+
+export default Header;
